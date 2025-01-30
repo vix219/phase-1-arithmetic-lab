52abf3ebf5c4cbed1fa75df7a3bb8b3223f349ce
VictoriaCarvalho <vix219@icloud.com>
2025-01-30 11:01:33 -0600

    add lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+     
+let random = 1;
+random++;
+random;
+
+let num3 = 8;
+let num4 = 2;
+let mod = (num3 / num4);
+const devide = num3 / num4;
+
+let max = 4 * 5;
+
